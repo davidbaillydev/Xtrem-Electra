7e8e4198f1f19a1cdf04a58feefb41be70a879ab
davidbaillydev <david.bailly8@gmail.com>
2023-04-21 18:20:56 +0200

    ok

---
+++ tina/templates.ts
@@ -0,0 +1,47 @@
+import type { TinaField } from "tinacms";
+export function v_lo__lectriqueFields() {
+  return [
+    {
+      type: "string",
+      name: "button_link",
+      label: "button_link",
+    },
+    {
+      type: "string",
+      name: "colors",
+      label: "colors",
+      list: true,
+    },
+    {
+      type: "datetime",
+      name: "date",
+      label: "date",
+    },
+    {
+      type: "string",
+      name: "description",
+      label: "description",
+    },
+    {
+      type: "string",
+      name: "discount_price",
+      label: "discount_price",
+    },
+    {
+      type: "image",
+      name: "images",
+      label: "images",
+      list: true,
+    },
+    {
+      type: "string",
+      name: "price",
+      label: "price",
+    },
+    {
+      type: "string",
+      name: "title",
+      label: "title",
+    },
+  ] as TinaField[];
+}
